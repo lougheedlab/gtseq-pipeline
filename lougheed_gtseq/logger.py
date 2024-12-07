5c8b37d3800b4b42f714a5ccf4fa47abcc574e4e
David Lougheed <david.lougheed@gmail.com>
2024-12-07 10:51:52 -0500

    chore: stop annoying matplotlib logspam

--- lougheed_gtseq/logger.py
+++ lougheed_gtseq/logger.py
@@ -5,6 +5,10 @@ __all__ = ["logger"]
 
 logging.basicConfig(level=logging.NOTSET)
 
+logging.getLogger("matplotlib").setLevel(logging.INFO)
+logging.getLogger("matplotlib.font_manager").setLevel(logging.INFO)
+logging.getLogger("matplotlib.pyplot").setLevel(logging.INFO)
+
 
 @functools.cache
 def get_logger() -> logging.Logger:
